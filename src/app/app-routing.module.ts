4b2ccbae9557adc4909262479ecf331c4533af31
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-05-02 10:14:20 +0500

    update files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GlavniComponent } from './companenta/glavni/glavni.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component: GlavniComponent},
+  {path: 'glavni', component: GlavniComponent},
+  {path: '**', component: GlavniComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
