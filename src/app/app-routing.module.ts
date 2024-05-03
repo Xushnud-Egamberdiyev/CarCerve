3aa822c2eb8b1dc6e7ed12fd039c35416b00fce9
Xushnud Egamberdiyev <140577111+Xushnud-Egamberdiyev@users.noreply.github.com>
2024-05-03 22:01:08 +0500

    adfsb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,18 @@ import { HammaServicelarComponent } from './companenta/hamma-servicelar/hamma-se
 import { NavbarComponent } from './companenta/navbar/navbar.component';
 import { BrendPloshadki1920Component } from './companenta/brend-ploshadki-1920/brend-ploshadki-1920.component';
 import { KartochkaBrendComponent } from './companenta/kartochka-brend/kartochka-brend.component';
+import { FooterComponent } from './companenta/footer/footer.component';
+import { AllServiceComponent } from './companenta/all-service/all-service.component';
+import { never } from 'rxjs';
+import { NewestComponent } from './companenta/newest/newest.component';
+import { OneProductComponent } from './companenta/one-product/one-product.component';
+import { ProductsComponent } from './companenta/products/products.component';
+import { NomlaComponent } from './companenta/company_create/nomla.component';
+import { ApplicationsComponent } from './companenta/applications/applications.component';
+import { InfoComponent } from './companenta/info/info.component';
+import { CommentsComponent } from './companenta/comments/comments.component';
+import { MyAutoComponent } from './companenta/my-auto/my-auto.component';
+import { PersInfoComponent } from './companenta/pers-info/pers-info.component';
 
 const routes: Routes = [
   {path : '', component: GlavniComponent},
@@ -15,6 +27,17 @@ const routes: Routes = [
   {path: 'navbar', component: NavbarComponent},
   {path: 'ploshadki', component: BrendPloshadki1920Component},
   {path: 'kartochka-brend', component: KartochkaBrendComponent},
+  {path: 'footer', component: FooterComponent},
+  {path: 'all_service', component: AllServiceComponent},
+  {path: 'newest', component: NewestComponent},
+  {path: 'one_product', component: OneProductComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'com_create', component: NomlaComponent},
+  {path: 'application', component: ApplicationsComponent},
+  {path: 'info', component: InfoComponent},
+  {path: 'comments', component: CommentsComponent},
+  {path: 'myAuto', component: MyAutoComponent},
+  {path: 'persInfo', component: PersInfoComponent},
   {path: '**', component: GlavniComponent},
 ];
 
